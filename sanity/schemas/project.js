ca2e3003d75f9341a477fb9723df3129da6fafcf
Lucas <lucasainestal@gmail.com>
2022-03-04 15:28:33 +0100

    in progress

---
+++ sanity/schemas/project.js
@@ -0,0 +1,31 @@
+export default {
+  name: "project",
+  title: "Project",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "mainImage",
+      title: "Main image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "url",
+      title: "Url",
+      type: "string",
+    },
+    {
+      name: "technologies",
+      title: "Technologies",
+      type: "array",
+      of: [{ type: "reference", to: { type: "technology" } }],
+    },
+  ],
+};
